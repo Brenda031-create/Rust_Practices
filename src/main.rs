036d42fd32814e0371a6d875516f5fa58c320fd0
brenda-create <brendathiaini31@gmail.com>
2026-05-08 19:46:09 +0300

    ownership

--- src/main.rs
+++ src/main.rs
@@ -26,4 +26,33 @@ fn main() {
     } else {
         println!("The number is small");
     }
+//else if practice
+    let a = 99;
+    if a > 200 {
+        println!("The number is huge");
+    } else if a > 99 {
+        println!("The number is big");
+    } else {
+        println!("The number is small");
+    }
+    //the code that will not work
+    if a > 99 {
+        println!("The number is big");
+    } else if a > 200 {
+        println!("The number is huge");
+    } else {
+        println!("The number is small");
+    }
+//match practice
+    let  number = 3;
+    match number {
+        1 => println!("The number is one"),
+        2 => println!("The number is two"),
+        3 => println!("The number is three"),
+        _ => println!("The number is something else"),
+    }
+//ownership practice
+    let s1 = String::from("Hello");
+    let s2 = s1; //s1 is moved to s2, s1 is no longer valid
+    println!("{}", s2);
 }
