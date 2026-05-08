d2f93bb602e68ee7e3e309e74db2f4fa08ac4662
brenda-create <brendathiaini31@gmail.com>
2026-05-08 15:37:30 +0300

    if else statement

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,15 @@ fn main() {
     let life: i32 =42;
     println!("The answer to the Ultimate Question of Life, The Universe, and Everything is: {}", life);
     println!("{:?}", life);
-    
+
     println!("{life}");
     println!("{life:?}");
+
+    //if else practice
+    let number = 70;
+    if number < 50 {
+        println!("The number is less than 50");
+    } else {
+        println!("The number is greater than 50");
+}
 }
