344c19a26171d5ae358a47cbeca7a9e91b2aaa2b
brenda-create <brendathiaini31@gmail.com>
2026-05-08 15:12:43 +0300

    println macro

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+    let life: i32 =42;
+    println!("The answer to the Ultimate Question of Life, The Universe, and Everything is: {}", life);
+    println!("{:?}", life);
+    
+    println!("{life}");
+    println!("{life:?}");
+}
